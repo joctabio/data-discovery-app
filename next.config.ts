57ad92483907e37e2ca03161389bed6811dffa10
joctabio <jacotabio@gmail.com>
2024-11-12 02:32:34 +0800

    Removed env variable in next.config.ts that was causing conflict with deployment

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  env: {
-    JSON_SERVER_ENDPOINT: "http://localhost:8080"
-  }
 };
 
 export default nextConfig;
